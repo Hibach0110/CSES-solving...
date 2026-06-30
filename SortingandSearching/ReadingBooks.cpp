fd89c764073b6ba6d8832d68486f5e1a3f633958
Hibach0110 <bachhoangluupham100@gmail.com>
2026-07-01 03:48:45 +0700

    hy vong qua tnthpt

---
+++ SortingandSearching/ReadingBooks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool used[200005];
+
+int main() {
+     ios_base::sync_with_stdio(0); cin.tie(0);
+     
+     int N; cin >> N;
+     vector<int> a(N);
+     for (int &x : a) cin >> x;
+     
+
+     return 0;
+}
--- SortingandSearching/TasksandDeadlines.cpp
+++ SortingandSearching/TasksandDeadlines.cpp
@@ -4,7 +4,19 @@ using namespace std;
 int main() {
      ios_base::sync_with_stdio(0); cin.tie(0);
      
-     
+     int N; cin >> N;
+     vector<int> a(N);
+     int64_t ans = 0;
+     for (int i = 0; i < N; i++) {
+          int D; cin >> a[i] >> D;
+          ans += D;
+     }
+     sort(a.begin(), a.end());
+     int cnt = N;
+     for (int i = 0; i < N; i++) {
+          ans -= 1ll * cnt * a[i]; cnt--;
+     }
+     cout << ans;
 
      return 0;
 }
