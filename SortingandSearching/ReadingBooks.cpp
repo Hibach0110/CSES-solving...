568d68781b56783a8b00fd188a6489867f1a8541
Hibach0110 <bachhoangluupham100@gmail.com>
2026-07-01 19:53:38 +0700

    add file

--- SortingandSearching/ReadingBooks.cpp
+++ SortingandSearching/ReadingBooks.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool used[200005];
-
 int main() {
      ios_base::sync_with_stdio(0); cin.tie(0);
      
      int N; cin >> N;
      vector<int> a(N);
      for (int &x : a) cin >> x;
-     
 
-     
+     sort(a.begin(), a.end());
+     int64_t sum = 0;
+     for (int i = 0; i < N-1; i++) sum += a[i];
+
+     if (sum <= a[N-1]) {
+          cout << 2ll * a[N-1];
+     } else {
+          sum += a[N-1];
+          cout << sum;
+     }
+
      return 0;
 }
