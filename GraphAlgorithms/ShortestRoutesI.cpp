ee4cd64f49c3f91ea6834b12552f3e0543a0da83
Hibach0110 <bachhoangluupham100@gmail.com>
2026-07-10 05:40:50 +0700

    early contribution

---
+++ GraphAlgorithms/ShortestRoutesI.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const int INF = 1e18;
+
+struct Edge {
+     int to, weight;
+};
+
+void solve() {
+     int n, m; cin >> n >> m;
+     vector<vector<Edge>> adj(n+1);
+     for (int i = 1; i <= m; i++) {
+          int u, v, l; cin >> u >> v >> l;
+          adj[u].pb({v, l});
+     }
+
+     vector<int> dist(n+1, INF);
+     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+     dist[1] = 0;
+     pq.push({0, 1});
+     while (!pq.empty()) {
+          auto [d, u] = pq.top();
+          pq.pop();
+          if (d > dist[u]) continue;
+          for (const auto& edge : adj[u]) {
+               int v = edge.to, w = edge.weight;
+               if (d + w < dist[v]) {
+                    dist[v] = d + w;
+                    pq.push({dist[v], v});
+               }
+          }
+     }
+
+     for (int i = 1; i <= n; i++)
+          cout << dist[i] << " ";
+}    
+
+int32_t main() {
+     ios_base::sync_with_stdio(false); cin.tie(NULL);
+     
+     #ifndef ONLINE_JUDGE
+     // freopen("input.txt", "r", stdin);
+     // freopen("output.txt", "w", stdout);
+     #endif
+
+     // cout << fixed << setprecision(12);
+
+     int tc = 1; //cin >> tc;
+     while (tc--) {
+          solve();
+     }
+
+     return 0;
+}
