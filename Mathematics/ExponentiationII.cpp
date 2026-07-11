90503f8a688e6df1da20a1501fccc26cad2dcd59
Hibach0110 <bachhoangluupham100@gmail.com>
2026-07-12 00:06:15 +0700

    ...

---
+++ Mathematics/ExponentiationII.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+    using ll = long long;
+
+void solve() {
+     
+}
+
+int main() {
+     ios_base::sync_with_stdio(false); cin.tie(NULL);  
+     #ifndef ONLINE_JUDGE
+     // freopen("input.txt", "r", stdin);
+     // freopen("output.txt", "w", stdout);
+     #endif
+     // cout << fixed << setprecision(12);
+
+     int tc = 1; cin >> tc;
+     while (tc--) {
+          solve();
+     }
+
+     return 0;
+}
